61cbcc03419fe9c0e96adfcfb4847aad644cbd04
Unknown <jsphillips2@wisc.edu>
2020-10-12 14:43:26 +0000

    Annual population projection and analysis

--- analysis/population_projection.R
+++ analysis/population_projection.R
@@ -10,6 +10,7 @@ options(mc.cores = parallel::detectCores()-4)
 out_in <- read_rds("output/fit_full.rds")
 
 # extract data
+data_prep <- out_in$data_prep
 data_list <- out_in$data_list
 fit <- out_in$fit 
 fit_summary <- out_in$fit_summary 
@@ -17,9 +18,9 @@ pj <- data_list$pj
 nt <- data_list$nt
 n <- data_list$n
 b <- data_list$b
-
-# select row
-id <- 50
+dates <- unique(data_prep$date)
+years_all <- lubridate::year(dates)
+years <- years_all[1:(length(years_all) - 2)]
 
 # paramter names for extraction
 vars <- {fit_summary %>%
@@ -34,159 +35,101 @@ vars <- {fit_summary %>%
            str_detect(fit_summary$var, "x0"))}$var
 
 # extract fit
-extract <-  rstan::extract(fit, pars = vars) %>%
+extract_full <-  rstan::extract(fit, pars = vars) %>%
   lapply(as_tibble) %>%
   bind_cols() %>%
   set_names(vars) %>%
-  filter(row_number() == id) %>%
-  gather(var, val) %>%
+  sample_n(1000) %>%
+  mutate(id = row_number()) %>%
+  gather(var, val, -id) %>%
   mutate(name = str_split(var, "\\[|\\]|,") %>% map_chr(~as.character(.x[1])),
          row = str_split(var, "\\[|\\]|,") %>% map_int(~as.integer(.x[2])),
          col = str_split(var, "\\[|\\]|,") %>% map_int(~as.integer(.x[3])))
 
+# select row
+id_ <- 50
 
-x0 <- extract %>%
-  filter(name == "x0") %>%
-  {array(.$val, dim = n)}
+extract <- extract_full %>%
+  filter(id == id_)
 
 
-rt <- extract %>%
-  filter(name == "rt") %>%
-  {array(.$val, dim = c(nt - 1, max(pj[, 1, 1]) - 1))} %>%
-  t()
 
-rf <- extract %>%
-  filter(name == "rf") %>%
-  {array(.$val, dim = c(max(pj[, 1 , 2]) - 1))}
 
-qt <- extract %>%
-  filter(name == "qt") %>%
-  {array(.$val, dim = c(nt - 1, max(pj[, 1 ,3]) - 1))} %>%
-  t()
 
-qf <- extract %>%
-  filter(name == "qf") %>%
-  {array(.$val, dim = c(max(pj[, 1 , 4]) - 1))}
 
-gt <- extract %>%
-  filter(name == "gt") %>%
-  {array(.$val, dim = c(nt - 1, max(pj[, 1 ,5]) - 1))} %>%
-  t()
 
-gf <- extract %>%
-  filter(name == "gf") %>%
-  {array(.$val, dim = c(max(pj[, 1 , 6]) - 1))}
+plot(colSums(x), type = "l", ylim = c(0, 17))
+points(colSums(data_list$y))
 
-dt <- extract %>%
-  filter(name == "dt") %>%
-  {array(.$val, dim = c(nt - 1, max(pj[, 1 ,7]) - 1))} %>%
-  t()
+years_unique <- unique(years)
+ny <- length(years_unique)
 
-df <- extract %>%
-  filter(name == "df") %>%
-  {array(.$val, dim = c(max(pj[, 1 , 8]) - 1))}
+
+aa <- lapply(1 : (ny - 1), function(i_){
+  ts <- sort(which(years == years_unique[i_]), decreasing = T)
   
+  Reduce("%*%", 
+         lapply(ts, function(t_){AA[,,t_]}))
   
-Rt <- matrix(0, nrow = n, ncol = n)
-Rf <- matrix(0, nrow = n, ncol = n)
-Qt <- matrix(0, nrow = n, ncol = n)
-Qf <- matrix(0, nrow = n, ncol = n)
-Gt <- matrix(0, nrow = n, ncol = n)
-Gf <- matrix(0, nrow = n, ncol = n)
-Dt <- matrix(0, nrow = n, ncol = n)
-Df <- matrix(0, nrow = n, ncol = n)
+})
 
-RR <- array(0, dim = c(n, n, nt - 1))
-QQ <- array(0, dim = c(n, n, nt - 1))
-GG <- array(0, dim = c(n, n, nt - 1))
-DD <- array(0, dim = c(n, n, nt - 1))
-AA <- array(0, dim = c(n, n, nt - 1))
-x <- array(0, dim = c(n, nt))
+aa_array <- aa %>% unlist() %>% array(dim = c(n, n, (ny - 1)))
 
-trans_fn <- function(m0_){
-  m1_ = m0_
-  diag(m1_) <- 0
-  v_ <- colSums(m0_)
-  m2_ <- diag(v_)
-  m3_ <- m1_ - m2_
-  m4_ <- expm(m3_)
-  return(as.matrix(m4_))
+n_proj <- array(0, dim = c(n, ny - 1))
+
+n_proj[, 1] <- x[, 1]
+
+for (y in 2 : (ny - 1)) {
+  
+  n_proj[, y] <- aa_array[, , y - 1] %*% n_proj[, y - 1]
+  
 }
 
-x[,1] <- x0
+plot(colSums(n_proj), type = "l")
 
-for (t in 2:nt) {
-  for (i in 1 : (n * n)) {
-    Rt[pj[i, 2, 1], pj[i, 3, 1]] <- c(0, rt[, t - 1])[pj[i, 1, 1]]
-    Rf[pj[i, 2, 2], pj[i, 3, 2]] <- c(0, rf)[pj[i, 1, 2]]
-    Qt[pj[i, 2, 3], pj[i, 3, 3]] <- c(0, qt[, t - 1])[pj[i, 1, 3]]
-    Qf[pj[i, 2, 2], pj[i, 3, 4]] <- c(0, qf)[pj[i, 1, 4]]
-    Gt[pj[i, 2, 5], pj[i, 3, 5]] <- c(0, gt[, t - 1])[pj[i, 1, 5]]
-    Gf[pj[i, 2, 6], pj[i, 3, 6]] <- c(0, gf)[pj[i, 1, 6]]
-    Dt[pj[i, 2, 7], pj[i, 3, 7]] <- c(0, dt[, t - 1])[pj[i, 1, 7]]
-    Df[pj[i, 2, 8], pj[i, 3, 8]] <- c(0, df)[pj[i, 1, 8]]
-    
-    R <- Rt + Rf
-    Q <- trans_fn(b[t - 1] * (Qt + Qf))
-    G <- trans_fn(b[t - 1] * (Gt + Gf))
-    D <- trans_fn(b[t - 1] * (Dt + Df))
-    
-    A =  R + (D %*% G %*% Q)
-    
-    x[, t] <- A %*% x[, t - 1]
-    
-    RR[, , t - 1] <- R
-    QQ[, , t - 1] <- Q
-    GG[, , t - 1] <- G
-    DD[, , t - 1] <- D
-    AA[, , t - 1] <- A
-    
-  }
-}
-
-AA
-
-
-
-surv <- lapply(2:nt, function(t_){
-  return(tibble(t = t_ - 1,
-                row = rep(1:n, n),
-                col = rep(1:n, each = n),
-                val = c(QQ[,,t_ - 1])))
-}) %>%
-  bind_rows() %>%
-  filter(row == col) %>%
-  mutate(basin = factor(ifelse(col < 3, "south", "north"),
-                        levels = c("south","north")),
-         stage = factor(ifelse(col %in% c(1, 3), "juvenile", "adult"),
-                        levels = c("juvenile","adult")))
-  
-
-surv %>%
-  ggplot(aes(t, val, color = basin))+
-  facet_wrap(~stage, nrow = 2)+
-  geom_line()+
-  scale_color_manual(values = c("dodgerblue","gray20"))+
-  theme_bw()
-
-
-disp <- lapply(2:nt, function(t_){
-  return(tibble(t = t_ - 1,
-                row = rep(1:n, n),
-                col = rep(1:n, each = n),
-                val = c(DD[,,t_ - 1])))
-}) %>%
-  bind_rows() %>%
-  filter(row %in% c(2, 4),
-         col %in% c(2, 4),
-         row != col) %>%
-  mutate(basin = factor(ifelse(col < 3, "south", "north"),
-                        levels = c("south","north")))
-
-
-disp %>%
-  ggplot(aes(t, val, color = basin))+
-  geom_line()+
-  scale_color_manual(values = c("dodgerblue","gray20"))+
-  theme_bw()+
-  scale_y_continuous(limits = c(0, 1))
+# AA
+# 
+# 
+# 
+# surv <- lapply(2:nt, function(t_){
+#   return(tibble(t = t_ - 1,
+#                 row = rep(1:n, n),
+#                 col = rep(1:n, each = n),
+#                 val = c(QQ[,,t_ - 1])))
+# }) %>%
+#   bind_rows() %>%
+#   filter(row == col) %>%
+#   mutate(basin = factor(ifelse(col < 3, "south", "north"),
+#                         levels = c("south","north")),
+#          stage = factor(ifelse(col %in% c(1, 3), "juvenile", "adult"),
+#                         levels = c("juvenile","adult")))
+# 
+# 
+# surv %>%
+#   ggplot(aes(t, val, color = basin))+
+#   facet_wrap(~stage, nrow = 2)+
+#   geom_line()+
+#   scale_color_manual(values = c("dodgerblue","gray20"))+
+#   theme_bw()
+# 
+# 
+# disp <- lapply(2:nt, function(t_){
+#   return(tibble(t = t_ - 1,
+#                 row = rep(1:n, n),
+#                 col = rep(1:n, each = n),
+#                 val = c(DD[,,t_ - 1])))
+# }) %>%
+#   bind_rows() %>%
+#   filter(row %in% c(2, 4),
+#          col %in% c(2, 4),
+#          row != col) %>%
+#   mutate(basin = factor(ifelse(col < 3, "south", "north"),
+#                         levels = c("south","north")))
+# 
+# 
+# disp %>%
+#   ggplot(aes(t, val, color = basin))+
+#   geom_line()+
+#   scale_color_manual(values = c("dodgerblue","gray20"))+
+#   theme_bw()+
+#   scale_y_continuous(limits = c(0, 1))
---
+++ analysis/population_projection_functions.R
@@ -0,0 +1,475 @@
+#========================================================================================= 
+# Annual population projection
+#=========================================================================================
+
+annual_proj_fn <- function(extract_, pj_, nt_, n_, b_, years_, theta_names_) {
+  
+  ### extract parameters
+  x0 <- extract_ %>%
+    filter(name == "x0") %>%
+    {array(.$val, dim = n)}
+  
+  
+  rt <- extract_ %>%
+    filter(name == "rt") %>%
+    {array(.$val, dim = c(nt - 1, max(pj_[, 1, 1]) - 1))} %>%
+    t()
+  
+  rf <- extract_ %>%
+    filter(name == "rf") %>%
+    {array(.$val, dim = c(max(pj_[, 1 , 2]) - 1))}
+  
+  qt <- extract_ %>%
+    filter(name == "qt") %>%
+    {array(.$val, dim = c(nt_ - 1, max(pj_[, 1 ,3]) - 1))} %>%
+    t()
+  
+  qf <- extract_ %>%
+    filter(name == "qf") %>%
+    {array(.$val, dim = c(max(pj_[, 1 , 4]) - 1))}
+  
+  gt <- extract_ %>%
+    filter(name == "gt") %>%
+    {array(.$val, dim = c(nt_ - 1, max(pj_[, 1 ,5]) - 1))} %>%
+    t()
+  
+  gf <- extract_ %>%
+    filter(name == "gf") %>%
+    {array(.$val, dim = c(max(pj_[, 1 , 6]) - 1))}
+  
+  dt <- extract_ %>%
+    filter(name == "dt") %>%
+    {array(.$val, dim = c(nt_ - 1, max(pj_[, 1 ,7]) - 1))} %>%
+    t()
+  
+  df <- extract_ %>%
+    filter(name == "df") %>%
+    {array(.$val, dim = c(max(pj_[, 1 , 8]) - 1))}
+  
+  
+  
+  
+  
+  ### define matrices for filling 
+  Rt <- matrix(0, nrow = n_, ncol = n_)
+  Rf <- matrix(0, nrow = n_, ncol = n_)
+  Qt <- matrix(0, nrow = n_, ncol = n_)
+  Qf <- matrix(0, nrow = n_, ncol = n_)
+  Gt <- matrix(0, nrow = n_, ncol = n_)
+  Gf <- matrix(0, nrow = n_, ncol = n_)
+  Dt <- matrix(0, nrow = n_, ncol = n_)
+  Df <- matrix(0, nrow = n_, ncol = n_)
+  
+  RR <- array(0, dim = c(n_, n_, nt_ - 1))
+  QQ <- array(0, dim = c(n_, n_, nt_ - 1))
+  GG <- array(0, dim = c(n_, n_, nt_ - 1))
+  DD <- array(0, dim = c(n_, n_, nt_ - 1))
+  AA <- array(0, dim = c(n_, n_, nt_ - 1))
+  x <- array(0, dim = c(n_, nt_))
+  
+  
+  
+  
+  #### define function transition probability matrix
+  trans_fn <- function(m0_){
+    m1_ = m0_
+    diag(m1_) <- 0
+    v_ <- colSums(m0_)
+    m2_ <- diag(v_)
+    m3_ <- m1_ - m2_
+    m4_ <- expm(m3_)
+    return(as.matrix(m4_))
+  }
+  
+  
+  
+  
+  ### set initial population size
+  x[,1] <- x0
+  
+  
+  
+  
+  
+  #### project over time
+  for (t in 2:nt_) {
+    for (i in 1 : (n_ * n_)) {
+      
+      # fill matrices
+      Rt[pj_[i, 2, 1], pj_[i, 3, 1]] <- c(0, rt[, t - 1])[pj_[i, 1, 1]]
+      Rf[pj_[i, 2, 2], pj_[i, 3, 2]] <- c(0, rf)[pj_[i, 1, 2]]
+      Qt[pj_[i, 2, 3], pj_[i, 3, 3]] <- c(0, qt[, t - 1])[pj_[i, 1, 3]]
+      Qf[pj_[i, 2, 2], pj_[i, 3, 4]] <- c(0, qf)[pj_[i, 1, 4]]
+      Gt[pj_[i, 2, 5], pj_[i, 3, 5]] <- c(0, gt[, t - 1])[pj_[i, 1, 5]]
+      Gf[pj_[i, 2, 6], pj_[i, 3, 6]] <- c(0, gf)[pj_[i, 1, 6]]
+      Dt[pj_[i, 2, 7], pj_[i, 3, 7]] <- c(0, dt[, t - 1])[pj_[i, 1, 7]]
+      Df[pj_[i, 2, 8], pj_[i, 3, 8]] <- c(0, df)[pj_[i, 1, 8]]
+      
+      # calculate projection matricees
+      R <- Rt + Rf
+      Q <- trans_fn(b_[t - 1] * (Qt + Qf))
+      G <- trans_fn(b_[t - 1] * (Gt + Gf))
+      D <- trans_fn(b_[t - 1] * (Dt + Df))
+      A =  R + (D %*% G %*% Q)
+      
+      # project dynamics
+      x[, t] <- A %*% x[, t - 1]
+      
+      # store projection matrices for export
+      RR[, , t - 1] <- R
+      QQ[, , t - 1] <- Q
+      GG[, , t - 1] <- G
+      DD[, , t - 1] <- D
+      AA[, , t - 1] <- A
+      
+    }
+  }
+  
+  
+  
+  
+  
+  # define unique years
+  years_unique <- unique(years_)
+  ny <- length(years_unique)
+  
+  
+  
+  
+  
+  ### annual projection matrix
+  aa <- lapply(1 : ny, function(i_){
+    ts <- sort(which(years_ == years_unique[i_]), decreasing = T)
+    
+    Reduce("%*%", 
+           lapply(ts, function(t_){AA[,,t_]}))
+    
+  })
+  aa_array <- aa %>% unlist() %>% array(dim = c(n_, n_, ny))
+  
+  
+  
+  
+  
+  #### project annual population size 
+  X_proj <- array(0, dim = c(n_, ny))
+  X_proj[, 1] <- x[, 1]
+  for (y in 2 : ny) {
+    
+    X_proj[, y] <- aa_array[, , y - 1] %*% X_proj[, y - 1]
+    
+  }
+  
+  
+  
+  
+  
+  ### extract parameter values
+  theta <- sapply(1:ny ,function(y){
+    ts_ <- which(years == years_unique[y])
+    c(RR[1,2,ts_[1]],RR[3,4,ts_[1]],
+      QQ[1,1,ts_[1]], QQ[2,2,ts_[1]], QQ[3,3,ts_[1]], QQ[4,4,ts_[1]],
+      GG[2,1,ts_[1]], DD[4,2,ts_[1]], DD[2,4,ts_[1]],
+      RR[1,2,ts_[2]],RR[3,4,ts_[2]],
+      QQ[1,1,ts_[2]], QQ[2,2,ts_[2]], QQ[3,3,ts_[2]], QQ[4,4,ts_[2]],
+      GG[2,1,ts_[2]], DD[4,2,ts_[2]], DD[2,4,ts_[2]])
+  }) %>% t()
+  colnames(theta) <- theta_names_
+  
+  return(list(RR = RR,
+              QQ = QQ,
+              GG = GG,
+              RR = RR,
+              aa = aa,
+              aa_array = aa_array,
+              X_proj = X_proj,
+              theta = theta,
+              n = n_, 
+              years = years_))
+
+}
+
+
+
+
+
+#========================================================================================= 
+# Derivatives in total abundance relative to matrix elements and parameters (theta) 
+#=========================================================================================
+
+dX_fn <- function(annual_proj_, ip_) {
+  
+  # define values from annual_proj
+  n = annual_proj_$n
+  years = annual_proj_$years
+  aa_array = annual_proj_$aa_array
+  X_proj = annual_proj_$X_proj
+  theta = annual_proj_$theta
+  
+  # define unique years
+  years_unique <- unique(years)
+  ny <- length(years_unique)
+  
+  # number of parameters
+  np <- ncol(theta)
+  
+  # identity matrix  
+  Is <- diag(nrow = n, ncol = n) 
+  
+  # initial state distribution
+  X <- X_proj[, 1]
+  X_asym <- X
+  
+  # initial derivatives of population size with respect to matrix elemetns
+  # set to 0
+  dXdA <- matrix(0, nrow = n, ncol = n^2) 
+  dXdT <- matrix(0, nrow = n, ncol = np) 
+  
+  # initialize lists for storing results
+  dXdA_out <- list()
+  dXdT_out <- list()
+  X_asym_out <- list()
+  X_out <- list()
+  
+  # loop over time
+  for (y in 1:ny) {
+    
+    # Store results
+    dXdA_out[[y]] = dXdA
+    dXdT_out[[y]] = dXdT 
+    X_asym_out[[y]] = X_asym
+    X_out[[y]] = X
+    
+    
+    dAdT <- matrix(sapply(1:(n* n), 
+                          function(j_){attributes(a_deriv_eval_fn(theta[y,], 
+                                                                  j_))$gradient}),
+                   ncol = ncol(theta),
+                   byrow = T)
+    
+    # loop over projection intervals
+    for (ipy in 1:ip_) {
+      
+      # project derivatives
+      dXdA = aa_array[, , y] %*% dXdA + kronecker(t(X), Is) 
+      
+      # project derivatives
+      dXdT = aa_array[, , y] %*% dXdT + kronecker(t(X), Is) %*% dAdT %*% diag(theta[y,])
+      
+      # Project abundances
+      X_asym = aa_array[, , y] %*% X_asym
+    }
+    
+    X = aa_array[, , y] %*% X
+    
+  }
+  
+  return(list(n = n,
+              years = years,
+              ip = ip_,
+              aa_array = aa_array,
+              dXdA_out = dXdA_out,
+              dXdT_out = dXdT_out,
+              X_asym_out = X_asym_out,
+              X_out = X_out))
+  
+}
+
+
+
+
+
+#========================================================================================= 
+# Sensitivity analysis
+#=========================================================================================
+
+sens_fn <- function(dX_) {
+  
+  # define values from dX
+  n = dX_$n
+  years = dX_$years
+  aa_array = dX_$aa_array
+  ip = dX_$ip
+  dXdA_out = dX_$dXdA_out
+  dXdT_out = dX_$dXdT_out
+  X_asym_out = dX_$X_asym_out
+  X_out = dX_$X_out
+  
+  # define unique years
+  years_unique <- unique(years)
+  ny <- length(years_unique)
+  
+  # vector for summing across states
+  c <- rep(1, n)
+  
+  # Loop over time
+  l_out <- lapply(2:ny, function(y){
+    
+    # proportional rate of increase (lambda)
+    l = (sum(X_asym_out[[y]])/sum(X_asym_out[[y - 1]]))^(1/ip) # realized 
+    l_asym = c(demogR::eigen.analysis(aa_array[, , y - 1])$lambda) # asymptotic
+    
+    # intrinsic growth rate
+    r = log(l) # realized 
+    r_asym = log(l_asym) # asymptotic
+    
+    
+    # sensitivity of realized r and lambda 
+    r_sens = t(((t(c) %*% dXdA_out[[y]])/sum(X_asym_out[[y]]) - 
+                   (t(c) %*% dXdA_out[[y - 1]])/sum(X_asym_out[[y - 1]])))
+    r_sens = r_sens/ip
+    l_sens = l * r_sens
+    
+    # sensitivity of asymptotic r and lambda 
+    l_sens_asym = c(demogR::eigen.analysis(aa_array[, , y - 1])$sensitivities)
+    r_sens_asym =  l_sens_asym/l_asym
+    
+    
+    # sensitivity of realized r and lambda with respect to pars
+    l_elas_p = t(((t(c) %*% dXdT_out[[y]])/sum(X_asym_out[[y]]) - 
+                     (t(c) %*% dXdT_out[[y - 1]])/sum(X_asym_out[[y - 1]])))
+    l_elas_p = l_elas_p / ip
+    r_elas_p = l_elas_p / l
+    
+    
+    # dataframe for export
+    list(l = l,
+         l_asym = l_asym,
+         r = r,
+         r_asym = r_asym,
+         l_sens = l_sens,
+         l_sens_asym = l_sens_asym,
+         r_sens = r_sens,
+         r_sens_asym = r_sens_asym,
+         l_elas_p = l_elas_p)
+  })
+  
+  names(l_out) = years_unique[1 : (ny - 1)]
+  return(l_out)
+}
+
+
+
+
+
+
+
+#========================================================================================= 
+# Test
+#=========================================================================================
+
+annual_proj <- annual_proj_fn(extract_ = extract, 
+                              pj_ = pj, 
+                              nt_ = nt, 
+                              n_ = n, 
+                              b_ = b, 
+                              years_ = years, 
+                              theta_names_ = theta_names) 
+
+
+
+dX <- dX_fn(annual_proj_ = annual_proj, 
+                    ip_ = 1)
+
+
+sens <- sens_fn(dX)
+
+
+
+
+
+
+l_elas_p <- sapply(sens, function(x_){
+  x_$l_elas_p 
+})
+
+l_elas_p_clean <- as_tibble(t(l_elas_p)) %>%
+  set_names(theta_names) %>%
+  mutate(time = row_number()) %>%
+  gather(var, val, -time) %>%
+  mutate(name = str_split(var, "") %>% map_chr(~as.character(.x[[1]])),
+         index = str_split(var, "") %>% map_chr(~as.character(.x[[2]])),
+         name = paste0(name, index),
+         season = str_split(var, "") %>% map_chr(~as.character(.x[[3]]))) %>%
+  group_by(name) %>%
+  arrange(name, time, season) %>%
+  mutate(season = ifelse(season == "s", 0, 1),
+         new_time = time + cumsum(season))
+
+
+l_elas_p_clean %>%
+  ggplot(aes(new_time, val))+
+  facet_wrap(~name)+
+  geom_hline(yintercept = 0, linetype = 2)+
+  geom_line(size = 1)+
+  theme_classic()
+
+l_elas_p_clean %>%
+  ggplot(aes(name, val, color = new_time))+
+  geom_hline(yintercept = 0, linetype = 2)+
+  geom_jitter(size = 2, height = 0, width = 0.1)+
+  theme_classic()
+
+
+l_elas_p_season <- l_elas_p_clean %>%
+  group_by(name, time) %>%
+  summarize(val = sum(val))
+
+l_elas_p_season %>%
+  ggplot(aes(time, val))+
+  facet_wrap(~name)+
+  geom_hline(yintercept = 0, linetype = 2)+
+  geom_line(size = 1)+
+  theme_classic()
+
+l_elas_p_season %>%
+  ggplot(aes(name, val, color = time))+
+  geom_hline(yintercept = 0, linetype = 2)+
+  geom_violin(linetype = 0, fill = "gray70", width = 1.5)+
+  geom_jitter(size = 2, height = 0, width = 0.1)+
+  theme_classic()
+
+
+
+
+l_elas_mean <- l_elas_p_season %>%
+  group_by(name) %>%
+  summarize(val = mean(val)) %>%
+  arrange(-val)
+
+l_elas_p_season %>%
+  ungroup() %>%
+  mutate(name = factor(name, 
+                       levels = l_elas_mean$name),
+         pos = as.numeric(name)) %>%
+  ggplot(aes(pos, val))+
+  geom_hline(yintercept = 0, linetype = 2)+
+  geom_jitter(aes(fill = time),
+              size = 2, height = 0, width = 0.1,
+              shape = 21, stroke = 0.5)+
+  geom_segment(data = l_elas_mean %>% 
+                 mutate(name = factor(name, 
+                                      levels = l_elas_mean$name),
+                        pos = as.numeric(name),
+                        xmin = pos - 0.2,
+                        xmax = pos + 0.2),
+               aes(x = xmin,
+                   y = val,
+                   xend = xmax,
+                   yend = val),
+               size = 2)+
+  scale_x_continuous("Demographic rate",
+                     breaks = 1:9,
+                     labels = l_elas_mean$name)+
+  scale_y_continuous(Proportional~sensitivity~of~lambda)+
+  scale_fill_gradient2(low ="dodgerblue", 
+                       mid="gray90", 
+                       high="firebrick", 
+                       midpoint = 15)+
+  coord_flip()+
+  theme_classic()
+
+
+
+
+
