b5da46966c517abd5e91036306fd2a9a5e806bfe
Unknown <jsphillips2@wisc.edu>
2020-10-14 09:07:53 +0000

    Updated model comparison (added to methods)

--- analysis/model_comparison.R
+++ analysis/model_comparison.R
@@ -1,43 +1,32 @@
+#==========
+#========== Preliminaries
+#==========
+
+# load packages
+library(tidyverse)
+library(rstan)
+library(loo)
+
+
 # import model
-name <- "full"
-out_full <- readRDS(
-  paste0("analysis/demographic_model/model/output/",name,"/fit_",name,".rds"))
-
-name <- "no_move"
-out_no_move <- readRDS(
-  paste0("analysis/demographic_model/model/output/",name,"/fit_",name,".rds"))
-
-
-
-# extract fit
-fit <- out_full$fit
-
-# summarize fit
-fit_summary <- rstan::summary(fit, probs=c(0.16, 0.5, 0.84))$summary %>%
-{as_tibble(.) %>%
-    mutate(var = rownames(rstan::summary(fit)$summary))}
+out_full <- read_rds("output/fit_full.rds")
+out_null <- read_rds("output/fit_null.rds")
+fit_full <- out_full$fit
+fit_null <- out_null$fit
+fit_summary_full <- out_full$fit_summary
+fit_summary_null <- out_null$fit_summary
 
 
 
-# extract fit
-fit_nm <- out_no_move$fit
-
-# summarize fit
-fit_summary_nm <- rstan::summary(fit_nm, probs=c(0.16, 0.5, 0.84))$summary %>%
-{as_tibble(.) %>%
-    mutate(var = rownames(rstan::summary(fit_nm)$summary))}
-
-fit_summary %>%
-  filter(var == "log_lik_sum")
 
 
-fit_summary_nm %>%
-  filter(var == "log_lik_sum")
 
-
+#==========
+#========== LOO & Log0likd
+#==========
 
 # loo
-loos <- lapply(c(fit,fit_nm), function(x){
+loos <- lapply(c(fit_full,fit_null), function(x){
   
   fit_ <- x
   
@@ -46,38 +35,122 @@ loos <- lapply(c(fit,fit_nm), function(x){
   
   loo <- loo(log_lik, r_eff = r_eff, cores = 10)
 }) %>%
-  set_names(c("fit","fit_nm"))
+  set_names(c("full","null"))
 
 # looic
-elpd <- compare(loos[[1]],
-                 loos[[2]])
+comp = {loo_compare(loos[[1]],
+                    loos[[2]]) %>%
+    as_tibble() %>%
+    as.matrix() %>%
+    as_tibble() %>%
+    mutate(model = c("full","null")[as.numeric(rownames(.))])} %>%
+  select(model, looic) %>%
+  mutate(looic = round(looic, 1),
+         loo_dev = round(looic - looic[1], 1)) %>%
+  full_join(tibble(model = c("full",
+                              "null"),
+                   loglik = c({fit_summary_full %>%
+                                filter(var == "log_lik_sum")}$`50%`,
+                              {fit_summary_null %>%
+                                  filter(var == "log_lik_sum")}$`50%`)))
 
 
 
 
 
-x_clean_nm <- fit_summary_nm %>%
+
+
+#==========
+#========== Plot
+#==========
+
+
+
+# extract scaling parameter
+y_scale <- out_null$y_scale
+
+# extract fit
+x_clean <- fit_summary_null %>%
   select(var, `16%`, `50%`, `84%`) %>%
   rename(lo = `16%`, mi = `50%`, hi = `84%`) %>%
-  filter(str_detect(fit_summary_nm$var, "x"), !str_detect(fit_summary_nm$var, "x0")) %>%
+  filter(str_detect(fit_summary_null$var, "x"), !str_detect(fit_summary_null$var, "x0")) %>%
   mutate(name = str_split(var, "\\[|\\]|,") %>% map_chr(~as.character(.x[1])),
          st = str_split(var, "\\[|\\]|,") %>% map_int(~as.integer(.x[2])),
          time = str_split(var, "\\[|\\]|,") %>% map_int(~as.integer(.x[3])),
          date = date_match[time]) %>%
   full_join(state_match) %>%
-  select(basin, stage, date, name, lo, mi, hi)
+  select(basin, state, stage, date, name, lo, mi, hi)
 
-x_clean %>%
-  mutate(model = "full") %>%
-  bind_rows(x_clean_nm %>%
-              mutate(model = "no_move")) %>%
-  ggplot(aes(date, mi))+
-  facet_grid(stage~basin)+
-  geom_line(data = data_prep, aes(x = date, mean_scale), size = 0.1)+
+
+
+
+
+#==========
+#========== Plot null
+#==========
+
+# plot labels
+labs <- x_clean %>%
+  tidyr::expand(stage) %>%
+  mutate(x = lubridate::as_date("2005-07-01"),
+         y = 10)
+
+# plot 
+p1 <-  ggplot(data = x_clean,
+              aes(x = date, 
+                  y = mi, 
+                  color = basin, 
+                  fill = basin))+
+  facet_rep_wrap(~stage, 
+                 nrow = 2)+
+  geom_text(data = labs,
+            aes(label = stage, 
+                x = x, 
+                y = y),
+            color = "black", 
+            size = 3,
+            inherit.aes = F)+
+  geom_ribbon(aes(ymin = lo, 
+                  ymax = hi),
+              alpha = 0.2,
+              linetype = 0)+
   geom_point(data = data_prep,
-             aes(x = date, mean_scale), size = 0.7, shape = 1, stroke = 0.3)+
-  geom_line(aes(x = date, y = mi,color = model), size = 0.4)+
-  scale_y_continuous("Abundance", breaks = c(300,900,1500))+
-  scale_x_date("Date", 
-               breaks = lubridate::as_date(c("1994-06-01","2004-06-01","2014-06-01")),
-               labels = c("1994","2004","2014"))
+             aes(y = mean_scale / y_scale),
+             shape = 1,
+             size = 0.8,
+             stroke = 0.35)+
+  geom_line(size = 0.35)+
+  scale_color_manual(name = "",
+                     values = basin_colors)+
+  scale_fill_manual(name = "",
+                    values = basin_colors,
+                    guide = F)+
+  scale_y_continuous(name = Relative~abundance,
+                     limits = c(0, 12),
+                     breaks = c(0, 3, 6, 9))+
+  scale_x_date(name = "Date",
+               limits = date_limits,
+               breaks = date_breaks,
+               labels = year_breaks)+
+  theme(panel.border = element_blank(),
+        panel.spacing = unit(-1, "lines"),
+        legend.key.height = unit(0.5, "lines"),
+        legend.key.width = unit(0.7, "lines"),
+        legend.position = c(0.15, 0.42),
+        strip.text.x = element_blank(),
+        axis.line.x = element_line(size = 0.25),
+        axis.line.y = element_line(size = 0.25))+
+  coord_capped_cart(left = "both", 
+                    bottom='both')+
+  guides(fill = guide_legend(override.aes = list(fill = NA)),
+         color = guide_legend(override.aes = list(size = 0.5,
+                                                  shape = NA)))
+
+# examine plot
+p1
+
+# export
+# cairo_pdf(file = "figures/figs/fig_fit_null.pdf",
+#           width = 3.5, height = 3.5, family = "Arial")
+# p1
+# dev.off()
